bb405ac1326972884f28f03c0efc603fab8911aa
Harsh Rohila <rohilaharsh@gmail.com>
2023-08-10 07:01:14 +0530

    added test for delete tracker

--- src/ui/action-btn/action-btn.tsx
+++ src/ui/action-btn/action-btn.tsx
@@ -8,7 +8,7 @@ interface ActionBtnProps {
 
 export function ActionBtn({ iconName, onBtnClick, title }: ActionBtnProps) {
   return (
-    <button className="action-btn" onClick={onBtnClick} title={title}>
+    <button className="action-btn" data-test={title?.toLowerCase()} onClick={onBtnClick} title={title}>
       <i class={`fa-solid fa-${iconName}`}></i>
     </button>
   )
