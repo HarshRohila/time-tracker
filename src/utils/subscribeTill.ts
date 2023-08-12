48c11cf0a47289012c5f5588ca88b83500108b83
Harsh Rohila <rohilaharsh@gmail.com>
2023-08-12 07:42:38 +0530

    fixed build error

--- src/utils/subscribeTill.ts
+++ src/utils/subscribeTill.ts
@@ -2,10 +2,10 @@ import { Observable, takeUntil } from "rxjs"
 
 export function untilDestroy(until$: Observable<unknown>) {
   return {
-    subscribe(stream: Observable<T>) {
+    subscribe<T>(stream: Observable<T>) {
       return stream.pipe(takeUntil(until$)).subscribe()
     },
-    stream(stream: Observable<T>) {
+    stream<T>(stream: Observable<T>) {
       return stream.pipe(takeUntil(until$))
     },
   }
